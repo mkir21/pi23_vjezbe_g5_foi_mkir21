a7c0d182b742aaee011cbc23c9f47f8d6e954592
mkir21 <mkir21@student.foi.hr>
2023-05-02 20:26:30 +0200

    Referencirana biblioteka za rad s bazom

--- Evaluation Manager (1)/Models/Student.cs
+++ Evaluation Manager (1)/Models/Student.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Evaluation_Manager.Models {
-    public class Student {
+    public class Student : Person{
         public int Grade { get; set; }
     }
 }
