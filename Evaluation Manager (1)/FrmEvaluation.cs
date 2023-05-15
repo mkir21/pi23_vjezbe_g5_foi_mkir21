2abad4bac306599fa794fd08bb78e213f9877e1e
mkir21 <mkir21@foi.hr>
2023-05-15 15:42:33 +0200

    Implementiran odabir aktivnosti na formi za evaluaciju

--- Evaluation Manager (1)/FrmEvaluation.cs
+++ Evaluation Manager (1)/FrmEvaluation.cs
@@ -1,4 +1,5 @@
 ﻿using Evaluation_Manager.Models;
+using Evaluation_Manager.Repositories;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,37 +14,34 @@ namespace Evaluation_Manager {
     public partial class FrmEvaluation : Form {
         private Student student;
 
-        //public FrmEvaluation() {
-        //    InitializeComponent();
-        //}
-
         public FrmEvaluation(Student selectedStudent) {
             InitializeComponent();
             student = selectedStudent;
         }
 
-        private void Form1_Load(object sender, EventArgs e) {
+        private void FrmEvaluation_Load(object sender, EventArgs e) {
+            SetFormText();
+            var activities = ActivityRepository.GetActivities();
+            cboActivities.DataSource = activities;
+        }
+        private void SetFormText() {
+            Text = student.FirstName + " " + student.LastName;
+        }
+
+        private void cboActivities_SelectedIndexChanged(object sender, EventArgs e) {
+            var currentActivity = cboActivities.SelectedItem as Activity;
+            txtActivityDescription.Text = currentActivity.Description;
+            txtMinForGrade.Text = currentActivity.MinPointsForGrade + "/" +
+           currentActivity.MaxPoints;
+            txtMinForSignature.Text = currentActivity.MinPointsForSignature + "/" +
+           currentActivity.MaxPoints;
+            numPoints.Minimum = 0;
+            numPoints.Maximum = currentActivity.MaxPoints;
 
         }
 
-        private void label1_Click(object sender, EventArgs e) {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e) {
-
-        }
-
-        private void label3_Click(object sender, EventArgs e) {
-
-        }
-
-        private void label5_Click(object sender, EventArgs e) {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e) {
-
+        private void btnCancel_Click(object sender, EventArgs e) {
+            Close();
         }
     }
 }
--- Evaluation Manager (1)/FrmStudents.cs
+++ Evaluation Manager (1)/FrmStudents.cs
@@ -30,11 +30,11 @@ namespace Evaluation_Manager
         private void ShowStudents() {
             List<Student> students = StudentRepository.GetStudents();
             dgvStudents.DataSource = students;
-
             dgvStudents.Columns["Id"].DisplayIndex = 0;
-            dgvStudents.Columns["FistName"].DisplayIndex = 1;
+            dgvStudents.Columns["FirstName"].DisplayIndex = 1;
             dgvStudents.Columns["LastName"].DisplayIndex = 2;
             dgvStudents.Columns["Grade"].DisplayIndex = 3;
+
         }
 
         private void btnEvaluateStudent_Click(object sender, EventArgs e) {
